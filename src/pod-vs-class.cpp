b54daa4c3a21c7c7e6a20b16a89664f993eca278
Keith Bennett <kanezor@gmail.com>
2017-09-19 21:43:51 -0500

    Quick update
    1. fix issue with last commit due to copy-pasting from another project ('me' does not exist)
    2. expose integer_t<T>::underlying_type
    3. allow implicit conversions of integer_t<T> to bool
    4. add some more missing operators
    5. add some static_asserts checking size of objects

--- src/pod-vs-class.cpp
+++ src/pod-vs-class.cpp
@@ -12,6 +12,8 @@ namespace pod_vs_class {
 
 		using integer_t = integer<t>;
 	public:
+		using underlying_type = T;
+
 		constexpr integer() = default;
 		constexpr integer(const integer & copy_ctor) = default;
 		constexpr integer(integer && move_ctor) = default;
@@ -19,24 +21,33 @@ namespace pod_vs_class {
 		constexpr integer & operator=(integer && move_assign) = default;
 
 		constexpr inline integer(T value) : value_(value) {}
+		constexpr inline operator bool() const { return value_; }
 
 		constexpr inline integer_t operator *(const integer_t & rhs) const { return value_ * rhs.value_; }
 		constexpr inline integer_t operator /(const integer_t & rhs) const { return value_ / rhs.value_; }
 		constexpr inline integer_t operator +(const integer_t & rhs) const { return value_ + rhs.value_; }
 		constexpr inline integer_t operator -(const integer_t & rhs) const { return value_ - rhs.value_; }
 		constexpr inline integer_t operator %(const integer_t & rhs) const { return value_ % rhs.value_; }
+		constexpr inline integer_t operator &(const integer_t & rhs) const { return value_ & rhs.value_; }
+		constexpr inline integer_t operator |(const integer_t & rhs) const { return value_ | rhs.value_; }
+		constexpr inline integer_t operator ^(const integer_t & rhs) const { return value_ ^ rhs.value_; }
+		constexpr inline integer_t operator <<(const integer_t & rhs) const { return value_ << rhs.value_; }
+		constexpr inline integer_t operator >>(const integer_t & rhs) const { return value_ >> rhs.value_; }
 
 		constexpr inline integer_t operator +() const { return +value_; }
 		constexpr inline integer_t operator -() const { return -value_; }
+		constexpr inline integer_t operator ~() const { return ~value_; }
 
-		constexpr inline me & operator *=(const me & rhs) { value_ *= rhs.value_; return *this; }
-		constexpr inline me & operator /=(const me & rhs) { value_ /= rhs.value_; return *this; }
-		constexpr inline me & operator +=(const me & rhs) { value_ += rhs.value_; return *this; }
-		constexpr inline me & operator -=(const me & rhs) { value_ -= rhs.value_; return *this; }
-		constexpr inline me & operator %=(const me & rhs) { value_ %= rhs.value_; return *this; }
-
-		constexpr inline me & operator +=(const me & rhs) const { value_ += rhs.value_; return *this; }
-		constexpr inline me & operator -=(const me & rhs) const { value_ -= rhs.value_; return *this; }
+		constexpr inline integer_t & operator *=(const integer_t & rhs) { value_ *= rhs.value_; return *this; }
+		constexpr inline integer_t & operator /=(const integer_t & rhs) { value_ /= rhs.value_; return *this; }
+		constexpr inline integer_t & operator +=(const integer_t & rhs) { value_ += rhs.value_; return *this; }
+		constexpr inline integer_t & operator -=(const integer_t & rhs) { value_ -= rhs.value_; return *this; }
+		constexpr inline integer_t & operator %=(const integer_t & rhs) { value_ %= rhs.value_; return *this; }
+		constexpr inline integer_t & operator &=(const integer_t & rhs) { value_ &= rhs.value_; return *this; }
+		constexpr inline integer_t & operator |=(const integer_t & rhs) { value_ |= rhs.value_; return *this; }
+		constexpr inline integer_t & operator ^=(const integer_t & rhs) { value_ ^= rhs.value_; return *this; }
+		constexpr inline integer_t & operator <<=(const integer_t & rhs) { value_ <<= rhs.value_; return *this; }
+		constexpr inline integer_t & operator >>=(const integer_t & rhs) { value_ >>= rhs.value_; return *this; }
 
 		constexpr inline integer_t & operator++() { ++value_; return *this; }
 		constexpr inline integer_t & operator--() { --value_; return *this; }
@@ -94,11 +105,39 @@ template <typename T>
 class ptr_t : public integer<T> {
 public:
 	using integer<T>::integer;
-	
 };
 
 using intptr_t = ptr_t<::std::intptr_t>;
 using uintptr_t = ptr_t<::std::uintptr_t>;
+
+static_assert(sizeof(int8_t) == sizeof(::std::int8_t), "sizeof(pod_vs_class::int8_t) != sizeof(::std::int8_t)");
+static_assert(sizeof(int16_t) == sizeof(::std::int16_t), "sizeof(pod_vs_class::int16_t) != sizeof(::std::int16_t)");
+static_assert(sizeof(int32_t) == sizeof(::std::int32_t), "sizeof(pod_vs_class::int32_t) != sizeof(::std::int32_t)");
+static_assert(sizeof(int64_t) == sizeof(::std::int64_t), "sizeof(pod_vs_class::int64_t) != sizeof(::std::int64_t)");
+static_assert(sizeof(int_fast8_t) == sizeof(::std::int_fast8_t), "sizeof(pod_vs_class::int_fast8_t) != sizeof(::std::int_fast8_t)");
+static_assert(sizeof(int_fast16_t) == sizeof(::std::int_fast16_t), "sizeof(pod_vs_class::int_fast16_t) != sizeof(::std::int_fast16_t)");
+static_assert(sizeof(int_fast32_t) == sizeof(::std::int_fast32_t), "sizeof(pod_vs_class::int_fast32_t) != sizeof(::std::int_fast32_t)");
+static_assert(sizeof(int_fast64_t) == sizeof(::std::int_fast64_t), "sizeof(pod_vs_class::int_fast64_t) != sizeof(::std::int_fast64_t)");
+static_assert(sizeof(int_least8_t) == sizeof(::std::int_least8_t), "sizeof(pod_vs_class::int_least8_t) != sizeof(::std::int_least8_t)");
+static_assert(sizeof(int_least16_t) == sizeof(::std::int_least16_t), "sizeof(pod_vs_class::int_least16_t) != sizeof(::std::int_least16_t)");
+static_assert(sizeof(int_least32_t) == sizeof(::std::int_least32_t), "sizeof(pod_vs_class::int_least32_t) != sizeof(::std::int_least32_t)");
+static_assert(sizeof(int_least64_t) == sizeof(::std::int_least64_t), "sizeof(pod_vs_class::int_least64_t) != sizeof(::std::int_least64_t)");
+
+static_assert(sizeof(uint8_t) == sizeof(::std::uint8_t), "sizeof(pod_vs_class::uint8_t) != sizeof(::std::uint8_t)");
+static_assert(sizeof(uint16_t) == sizeof(::std::uint16_t), "sizeof(pod_vs_class::uint16_t) != sizeof(::std::uint16_t)");
+static_assert(sizeof(uint32_t) == sizeof(::std::uint32_t), "sizeof(pod_vs_class::uint32_t) != sizeof(::std::uint32_t)");
+static_assert(sizeof(uint64_t) == sizeof(::std::uint64_t), "sizeof(pod_vs_class::uint64_t) != sizeof(::std::uint64_t)");
+static_assert(sizeof(uint_fast8_t) == sizeof(::std::uint_fast8_t), "sizeof(pod_vs_class::uint_fast8_t) != sizeof(::std::uint_fast8_t)");
+static_assert(sizeof(uint_fast16_t) == sizeof(::std::uint_fast16_t), "sizeof(pod_vs_class::uint_fast16_t) != sizeof(::std::uint_fast16_t)");
+static_assert(sizeof(uint_fast32_t) == sizeof(::std::uint_fast32_t), "sizeof(pod_vs_class::uint_fast32_t) != sizeof(::std::uint_fast32_t)");
+static_assert(sizeof(uint_fast64_t) == sizeof(::std::uint_fast64_t), "sizeof(pod_vs_class::uint_fast64_t) != sizeof(::std::uint_fast64_t)");
+static_assert(sizeof(uint_least8_t) == sizeof(::std::uint_least8_t), "sizeof(pod_vs_class::uint_least8_t) != sizeof(::std::uint_least8_t)");
+static_assert(sizeof(uint_least16_t) == sizeof(::std::uint_least16_t), "sizeof(pod_vs_class::uint_least16_t) != sizeof(::std::uint_least16_t)");
+static_assert(sizeof(uint_least32_t) == sizeof(::std::uint_least32_t), "sizeof(pod_vs_class::uint_least32_t) != sizeof(::std::uint_least32_t)");
+static_assert(sizeof(uint_least64_t) == sizeof(::std::uint_least64_t), "sizeof(pod_vs_class::uint_least64_t) != sizeof(::std::uint_least64_t)");
+
+static_assert(sizeof(intptr_t) == sizeof(::std::intptr_t), "sizeof(pod_vs_class::intptr_t) != sizeof(::std::intptr_t)");
+static_assert(sizeof(uintptr_t) == sizeof(::std::uintptr_t), "sizeof(pod_vs_class::uintptr_t) != sizeof(::std::uintptr_t)");
 
 } // namespace pod_vs_class
 
